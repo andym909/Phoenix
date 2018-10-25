baf11eb2aabc91e93e1169fa498ed974884a3e31
adamcantor22 <11340232+adamcantor22@users.noreply.github.com>
2018-10-25 08:33:36 -0400

    shooting

---
+++ Phoenix/Assets/Scripts/Arrow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+	Vector3 target;
+	float speed = 5f;
+
+	public void SetTarget(Vector3 t) {
+		target = t;
+	}
+
+	void Update() {
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+		if(transform.position == target) {
+			Destroy(gameObject);
+		}
+	}
+}
