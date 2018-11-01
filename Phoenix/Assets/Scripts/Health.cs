54b6036a1ea09d718fd4516e1bf1bef6d96133bb
Adam Cantor <adamcantor22@gmail.com>
2018-11-01 11:40:29 -0400

    Merchant & Health
    Merchant can be placed in scene, health exists but doesn't do anything

---
+++ Phoenix/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	int health;
+
+	public void SetHealth(int start) {
+		health = start;
+	}
+
+	public int GetHealth() {
+		return health;
+	}
+
+	public void GainHealth(int delta) {
+		health += delta;
+	}
+
+	public void LoseHealth(int delta) {
+		health -= delta;
+	}
+
+	public bool IsAlive() {
+		return health > 0;
+	}
+}
---
+++ Phoenix/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : Health {
+
+	public int startingHealth;
+
+	void Start () {
+		SetHealth(startingHealth);
+	}
+
+	void Update () {
+		if(!IsAlive()) {
+			Destroy(gameObject);
+		}
+	}
+}
