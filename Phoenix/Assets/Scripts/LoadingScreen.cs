8690abfb9eddf3180c6a60b27191de9f243fd1a7
Adam Cantor <adamcantor22@gmail.com>
2018-12-03 21:22:30 -0500

    Loading Screen Works

--- Phoenix/Assets/Scripts/LoadingScreen.cs
+++ Phoenix/Assets/Scripts/LoadingScreen.cs
@@ -6,15 +6,43 @@ using UnityEngine.UI;
 public class LoadingScreen : MonoBehaviour {
 
 	public Image load;
+	GameObject canvas;
 
 	public bool loading = false;
 
+	void Start() {
+		canvas = GameObject.Find("Canvas");
+	}
+
 	public void LoadScreen() {
+		Image[] images = canvas.GetComponentsInChildren<Image>();
+		for(int i = 0; i < images.Length; i++) {
+			if(!images[i].Equals(load))
+				images[i].enabled = false;
+		}
+		Text[] texts = canvas.GetComponentsInChildren<Text>();
+		for(int i = 0; i < texts.Length; i++) {
+			texts[i].enabled = false;
+		}
+
 		load.enabled = true;
 		loading = true;
 	}
 
 	public void FinishLoad() {
+		if(canvas == null) {
+			canvas = GameObject.Find("Canvas");
+		}
+		Image[] images = canvas.GetComponentsInChildren<Image>();
+		for(int i = 0; i < images.Length; i++) {
+			if(!images[i].Equals(load))
+				images[i].enabled = true;
+		}
+		Text[] texts = canvas.GetComponentsInChildren<Text>();
+		for(int i = 0; i < texts.Length; i++) {
+			texts[i].enabled = true;
+		}
+
 		load.enabled = false;
 		loading = false;
 	}
