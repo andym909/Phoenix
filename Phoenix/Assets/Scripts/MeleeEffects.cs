c63299898c75bd1dc1b1aa0a1ea7a8fea3b4e106
Adam Cantor <adamcantor22@gmail.com>
2018-12-06 11:44:07 -0500

    Visual Tweaks

--- Phoenix/Assets/Scripts/MeleeEffects.cs
+++ Phoenix/Assets/Scripts/MeleeEffects.cs
@@ -6,12 +6,14 @@ public class MeleeEffects : MonoBehaviour {
 
 	Player player;
 	Animator anim;
+	SpriteRenderer sprite;
 	int facing;
 	float offset = 0.65f;
 
 	void Start() {
 		player = GetComponentInParent<Player>();
 		anim = GetComponentInChildren<Animator>();
+		sprite = GetComponentInChildren<SpriteRenderer>();
 	}
 
 	void Update() {
@@ -23,18 +25,22 @@ public class MeleeEffects : MonoBehaviour {
 		Vector3 parentPos = transform.parent.transform.position;
 		switch(facing) {
 			case 0:
-				transform.position = new Vector3(parentPos.x, parentPos.y + offset);
+				sprite.sortingLayerName = "Items";
+				transform.position = new Vector3(parentPos.x, parentPos.y + offset * 1.2f);
 				transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
 				break;
 			case 1:
+				sprite.sortingLayerName = "Items";
 				transform.position = new Vector3(parentPos.x + offset, parentPos.y);
 				transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -90f));
 				break;
 			case 2:
+				sprite.sortingLayerName = "Units";
 				transform.position = new Vector3(parentPos.x, parentPos.y - offset);
 				transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 180f));
 				break;
 			case 3:
+				sprite.sortingLayerName = "Items";
 				transform.position = new Vector3(parentPos.x - offset, parentPos.y);
 				transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));
 				break;
