9e75a85d453399a535ce4f700b81e6f94f338785
Andy <andymartin909@gmail.com>
2018-12-13 16:41:36 -0500

    walls
    removed stuff from tutorial, added a comment

--- Phoenix/Assets/Scripts/Wall.cs
+++ Phoenix/Assets/Scripts/Wall.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour {
 
-    public Sprite dmgSprite;
-    public int hp = 4;
-
     private SpriteRenderer spriteRenderer;
 
-	// Use this for initialization
+	// render the walls
 	void Awake () {
         spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
-    public void Damagewall(int loss) {
-        spriteRenderer.sprite = dmgSprite;
-        hp -= loss;
-        if (hp <= 0)
-            gameObject.SetActive(false);
-    }
 }
