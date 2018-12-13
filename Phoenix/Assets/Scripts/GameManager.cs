80cacf2172fe651ccce49438032b5f14d4800121
Drew Verenna <dverenna@gmail.com>
2018-12-13 14:49:19 -0500

    Commented GameManager and Parts of Player

--- Phoenix/Assets/Scripts/GameManager.cs
+++ Phoenix/Assets/Scripts/GameManager.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+/*
+ *  GameManager: The script that controls the initialization of the game, increasing the level, and restarting the game
+ */
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance = null;
@@ -30,28 +32,31 @@ public class GameManager : MonoBehaviour {
 	}
 
     public void InitGame() {
-        BoardCreator.level = GameManager.level;
-        setEnemyHealth();
-        setEnemyDamage();
-        boardScript.SetupScene();
+        BoardCreator.level = GameManager.level;   // Make sure that the board creator is on the correct level
+        setEnemyHealth();                         // Logarithmically increase the enemy health
+        setEnemyDamage();                         // Logarithmically increase the enemy damage
+        boardScript.SetupScene();                 // Set up the next level for the player to be in
     }
 
     public void GameOver() {
-        GameManager.level = 1;
-        Invoke("Restart", 3.5f);
+        GameManager.level = 1;                    // Set the level back to one when the player dies
+        Invoke("Restart", 3.5f);                  // Restart the game
         enabled = false;
     }
 
+    //Logarithmically increase the enemy health
     public void setEnemyHealth() {
         boardScript.enemy1.GetComponent<Health>().setStartingHealth((int)Mathf.Floor(3 * Mathf.Log(level) + 3));
         boardScript.enemy2.GetComponent<Health>().setStartingHealth((int)Mathf.Floor(3 * Mathf.Log(level) + 3));
     }
 
+    //Logarithmically increase the enemy damage
     public void setEnemyDamage() {
         boardScript.enemy1.GetComponent<ChaseAttack>().setDamage(level / 3 + 1);
         boardScript.enemy2.GetComponent<ShootingAttack>().setDamage(level / 3 + 1);
     }
 
+    //Go back to the title screen
     public void Restart() {
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);	
     }
